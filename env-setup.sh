669ef04a368720aedb9743bbaf833c28eb787dbf
Anil Kumar Chhetri <59021082+anil-chhetri@users.noreply.github.com>
2025-03-17 21:32:01 +0000

    add file for automate installation of rust in github codespaces.

---
+++ env-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export PS1="\\w> "
+
+#source ~/path/to/shell-functions.sh
+# echo "source /workspaces/Rust-Learning/env-setup.sh"  >> ~/.bashrc
+
+
+if ! command -v rustc &>/dev/null; then
+    echo "Rust not found. Installing Rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
