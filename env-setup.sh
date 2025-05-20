7594fbb3d7add4b5155b9b037ce7de42e9181b1b
Anil Kumar Chhetri <59021082+anil-chhetri@users.noreply.github.com>
2025-05-20 20:38:51 +0000

    removing default rust installation.

--- env-setup.sh
+++ env-setup.sh
@@ -5,7 +5,7 @@ export PS1="\\W> "
 # echo "source /workspaces/Rust-Learning/env-setup.sh"  >> ~/.bashrc
 
 
-if ! command -v rustc &>/dev/null; then
-    echo "Rust not found. Installing Rust..."
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-fi
+# if ! command -v rustc &>/dev/null; then
+#     echo "Rust not found. Installing Rust..."
+#     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# fi
