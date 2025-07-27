2d8ee6de39542db471e7c2d446f650db93c0f038
Anil Kumar Chhetri <59021082+anil-chhetri@users.noreply.github.com>
2025-07-27 16:26:50 +0000

    scan subcommand completed.

--- forg/src/main.rs
+++ forg/src/main.rs
@@ -1,5 +1,56 @@
 use clap::{ArgAction, Parser, Subcommand};
 
+use std::{
+    collections::{HashMap},
+    fs,
+    io::Error,
+    path::{Path, PathBuf},
+};
+
+fn pre_validation(path: &String) -> bool {
+    Path::new(path).is_dir()
+}
+
+fn get_files(path: &String) -> Result<Vec<PathBuf>, Error> {
+    if !pre_validation(path) {
+        return Ok(Vec::new());
+    }
+
+    let mut all_files: Vec<PathBuf> = Vec::new();
+
+    for entry_result in fs::read_dir(path)? {
+        let entry = entry_result?;
+
+        let file_type = entry.file_type()?;
+
+        if file_type.is_dir() {
+            continue;
+        }
+        all_files.push(entry.path());
+    }
+    Ok(all_files)
+}
+
+fn scan_files(path: &String) -> Result<String, Error> {
+    let all_files = get_files(path)?;
+
+    if all_files.len() == 0 {
+        println!("no files found in {} directory.", path);
+    }
+    let mut scan_result = HashMap::new();
+    
+    for file in all_files{
+        
+        if let Some(extension) = file.extension().and_then(|e| e.to_str()){
+            *scan_result.entry(extension.to_string()).or_insert(0) += 1;
+        }
+    }
+
+    println!("{:?}", scan_result);
+
+    Ok("test".to_string())
+}
+
 #[derive(Debug, Parser)]
 #[command(version, author, long_about = "file organization app")]
 struct Forg {
@@ -10,11 +61,17 @@ struct Forg {
         ,help="provides the preview of changes with making any changes."
     )]
     preview: bool,
+
+    #[arg(long, short, help("should look through sub folder or not.")
+    , action = ArgAction::SetTrue
+    )]
+    recursive: bool,
 }
 
 #[derive(Debug, Subcommand)]
 enum ForgOptions {
     #[command(
+        arg_required_else_help(true),
         long_about = "scan the given directory and provide information on each extension found."
     )]
     Scan {
@@ -27,5 +84,17 @@ enum ForgOptions {
 fn main() {
     let fo = Forg::parse();
 
-    println!("{:?}", fo);
+    match fo.forg_option {
+        ForgOptions::Organize => {
+            println!("received organize");
+        }
+        ForgOptions::Scan { file_path } => {
+            // println!("Recived scan with file_path: {}", file_path);
+            // println!("preview flag: {}", fo.preview);
+            // println!("recursive flag: {}", fo.recursive);
+
+            // get_files(&file_path).expect("can't process the file path");
+            scan_files(&file_path).expect("can' t process file in directory");
+        }
+    }
 }
